7d908d8d860933befae7314ea6132f44fd7eaeff
ivan-icko <ivanstepanovic6@gmail.com>
2022-03-18 21:13:06 +0100

    Dokumentovan interfejs Radnja.

--- VulkanizerskaRadnja/src/auto_radnja/Radnja.java
+++ VulkanizerskaRadnja/src/auto_radnja/Radnja.java
@@ -3,10 +3,32 @@ package auto_radnja;
 import java.util.List;
 import auto_radnja.gume.AutoGuma;
 
+
+/**
+ * 
+ * @author Ivan
+ * @version 1.0
+ *
+ */
 public interface Radnja {
+	
+	/**
+	 * Dodaje novu gumu unetu kao parametar metode
+	 * @param a AutoGuma
+	 */
 	void dodajGumu(AutoGuma a);
 
+	
+	/**
+	 * Pretrazuje AutoGume preme markaModelui vraca listu objekata klase AutoGuma
+	 * @param markaModel
+	 * @return List<AutoGuma> lista
+	 */
 	List<AutoGuma> pronadjiGumu(String markaModel);
 
+	/**
+	 * Vraca listu svih AutoGuma
+	 * @return List<AutoGuma>
+	 */
 	List<AutoGuma> vratiSveGume();
 }
